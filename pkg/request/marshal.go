74013285b0557177deeea620de259f909987b15c
yuanbohan <yuanbo.han@gmail.com>
2023-02-20 19:25:47 +0800

    feat(insert): insert into greptimedb

---
+++ pkg/request/marshal.go
@@ -0,0 +1,12 @@
+package request
+
+import (
+	greptime "github.com/GreptimeTeam/greptime-proto/go/greptime/v1"
+)
+
+// FIXME(yuanbohan): the real logic
+// marshal convert struct to column slice.
+func marshal(data any) ([]greptime.InsertRequest, error) {
+
+	return nil, nil
+}
