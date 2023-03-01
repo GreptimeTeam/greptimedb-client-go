5ee3bcfdac5dad7bb0b77ce9eb04de12b4b5768d
Bohan Wu <79818743+vinland-avalon@users.noreply.github.com>
2023-03-01 10:39:04 +0800

    test: fix unittests (#26)

--- pkg/request/metric_test.go
+++ pkg/request/metric_test.go
@@ -137,9 +137,10 @@ func TestMetricSemanticNotMatch(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
-// 8 columns
-// row1: 4 tags, 2 fields (with 2 null column)
+// 9 columns
+// row1: 4 tags, 2 fields (with 2 null column), 1 timestamp, named as default "ts"
 // row2: 2 tags, 4 fields (with 2 null column)
+// the timstamp column should be at last
 func TestGreptimeColumn(t *testing.T) {
 	timestamp := time.Now()
 
--- pkg/sql/dsn_test.go
+++ pkg/sql/dsn_test.go
@@ -1,12 +1,12 @@
 package sql
 
 import (
-	req "github.com/GreptimeTeam/greptimedb-client-go/pkg/request"
 	"reflect"
 	"testing"
+
+	req "github.com/GreptimeTeam/greptimedb-client-go/pkg/request"
 )
 
-// TODO: more test cases
 var testDSNs = []struct {
 	in  string
 	out *req.Config
@@ -14,14 +14,47 @@ var testDSNs = []struct {
 	"username:password@protocol(address)/catalogname:dbname",
 	&req.Config{UserName: "username", Password: "password", Net: "protocol", Address: "address", Catalog: "catalogname", Database: "dbname"},
 }, {
+	"username:password@protocol(address)/catalogname:",
+	&req.Config{UserName: "username", Password: "password", Net: "protocol", Address: "address", Catalog: "catalogname", Database: ""},
+}, {
 	"username:password@protocol(address)/dbname",
 	&req.Config{UserName: "username", Password: "password", Net: "protocol", Address: "address", Catalog: "", Database: "dbname"},
+}, {
+	"username:password@protocol(address)/",
+	&req.Config{UserName: "username", Password: "password", Net: "protocol", Address: "address", Catalog: "", Database: ""},
+}, {
+	"username:password@protocol/dbname",
+	&req.Config{UserName: "username", Password: "password", Net: "protocol", Address: "127.0.0.1:4001", Catalog: "", Database: "dbname"},
+}, {
+	"username:password@(address)/dbname",
+	&req.Config{UserName: "username", Password: "password", Net: "", Address: "address", Catalog: "", Database: "dbname"},
+}, {
+	"username:password@/dbname",
+	&req.Config{UserName: "username", Password: "password", Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: "dbname"},
+}, {
+	"username@/dbname",
+	&req.Config{UserName: "username", Password: "", Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: "dbname"},
+}, {
+	":password@/dbname",
+	&req.Config{UserName: "", Password: "password", Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: "dbname"},
+}, {
+	"/dbname",
+	&req.Config{UserName: "", Password: "", Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: "dbname"},
 }, {
 	"/",
 	&req.Config{Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: ""},
 }, {
-	"(address)/",
-	&req.Config{Net: "", Address: "address", Catalog: "", Database: ""},
+	"",
+	&req.Config{Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: ""},
+}, {
+	"username:p@ssword@/dbname",
+	&req.Config{UserName: "username", Password: "p@ssword", Net: "", Address: "127.0.0.1:4001", Catalog: "", Database: "dbname"},
+}, {
+	"/catalogname::::dbname",
+	&req.Config{UserName: "", Password: "", Net: "", Address: "127.0.0.1:4001", Catalog: "catalogname", Database: ":::dbname"},
+}, {
+	"(protocal)(address)/dbname",
+	&req.Config{UserName: "", Password: "", Net: "", Address: "protocal)(address", Catalog: "", Database: "dbname"},
 },
 }
 
