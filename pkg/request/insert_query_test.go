45f1505d0430e44ddae6ac011d2591ae4011506e
BohanWu <bohanwu1022@gmail.com>
2023-03-13 21:54:58 +0800

    test: unit tests

--- pkg/request/insert_query_test.go
+++ pkg/request/insert_query_test.go
@@ -29,9 +29,14 @@ var (
 type datatype struct {
 	int64V   int64
 	int32V   int32
+	int16V   int16
+	int8V    int8
 	uint64V  uint64
 	uint32V  uint32
+	uint16V  uint16
+	uint8V   uint8
 	float64V float64
+	float32V float32
 	stringV  string
 	byteV    []byte
 	boolV    bool
@@ -92,9 +97,14 @@ func TestBasicWorkFlow(t *testing.T) {
 	data := datatype{
 		int64V:   64,
 		int32V:   32,
+		int16V:   16,
+		int8V:    8,
 		uint64V:  64,
 		uint32V:  32,
+		uint16V:  16,
+		uint8V:   8,
 		float64V: 64.0,
+		float32V: 32.0,
 		stringV:  "string",
 		byteV:    []byte("byte"),
 		boolV:    true,
@@ -116,9 +126,14 @@ func TestBasicWorkFlow(t *testing.T) {
 	series := Series{}
 	series.AddTag("int64_v", data.int64V)
 	series.AddTag("int32_v", data.int32V)
+	series.AddTag("int16_v", data.int16V)
+	series.AddTag("int8_v", data.int8V)
 	series.AddTag("uint64_v", data.uint64V)
 	series.AddField("uint32_v", data.uint32V)
+	series.AddField("uint16_v", data.uint16V)
+	series.AddField("uint8_v", data.uint8V)
 	series.AddField("float64_v", data.float64V)
+	series.AddField("float32_v", data.float32V)
 	series.AddField("string_v", data.stringV)
 	series.AddField("byte_v", data.byteV)
 	series.AddField("bool_v", data.boolV)
@@ -145,11 +160,21 @@ func TestBasicWorkFlow(t *testing.T) {
 	assert.True(t, ok)
 	int32V, ok := series.Get("int32_v")
 	assert.True(t, ok)
+	int16V, ok := series.Get("int16_v")
+	assert.True(t, ok)
+	int8V, ok := series.Get("int8_v")
+	assert.True(t, ok)
 	uint64V, ok := series.Get("uint64_v")
 	assert.True(t, ok)
 	uint32V, ok := series.Get("uint32_v")
 	assert.True(t, ok)
+	uint16V, ok := series.Get("uint16_v")
+	assert.True(t, ok)
+	uint8V, ok := series.Get("uint8_v")
+	assert.True(t, ok)
 	float64V, ok := series.Get("float64_v")
+	assert.True(t, ok)
+	float32V, ok := series.Get("float32_v")
 	assert.True(t, ok)
 	stringV, ok := series.Get("string_v")
 	assert.True(t, ok)
@@ -163,9 +188,14 @@ func TestBasicWorkFlow(t *testing.T) {
 	querydata := datatype{
 		int64V:   int64V.(int64),
 		int32V:   int32V.(int32),
+		int16V:   int16(int16V.(int32)),
+		int8V:    int8(int8V.(int32)),
 		uint64V:  uint64V.(uint64),
 		uint32V:  uint32V.(uint32),
+		uint16V:  uint16(uint16V.(uint32)),
+		uint8V:   uint8(uint8V.(uint32)),
 		float64V: float64V.(float64),
+		float32V: float32(float32V.(float64)),
 		stringV:  stringV.(string),
 		byteV:    []byte(byteV.(string)),
 		boolV:    boolV.(bool),
